32c964a36cbc03f0bca3ef69c36db5fb6431019b
José Misael Adame Sandoval <misael_adame@protonmail.com>
2021-01-12 16:34:38 -0600

    Add files via upload

---
+++ Paginas U5/en_clase/mi_primer_php.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mi Primer Pagina PHP</title>
+</head>
+<body>
+    <h3>Esta pagina incluye un Script de PHP</h3>
+    <br>
+    <?php
+        phpinfo(); 
+    ?>
+</body>
+</html>
